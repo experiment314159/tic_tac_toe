bc95c72e2983ee2742f31e6c44bf704f6cc2ec5d
experiment314159 <yousef.hisham444@proton.me>
2024-07-13 17:59:24 +0200

    5

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 mod tic_tac_toe;
 #[allow(unused_imports)]
-use tic_tac_toe::{GamePos, NONE, O, X};
 
 fn main() {
 
--- src/tic_tac_toe.rs
+++ src/tic_tac_toe.rs
@@ -1,9 +1,11 @@
+use std::collections::HashMap;
+
 pub type Player = u8;
 pub type GamePos = [Player; 9];
 
-pub const NONE: Player = 1;
-pub const X: Player = 2;
-pub const O: Player = 3;
+pub const NONE: Player = 0;
+pub const X: Player = 1;
+pub const O: Player = 2;
 const WINNING_PATTERNS: [[u8; 3]; 8] = [
     [0, 1, 2],
     [3, 4, 5],
@@ -16,26 +18,6 @@ const WINNING_PATTERNS: [[u8; 3]; 8] = [
 ];
 
 #[allow(dead_code)]
-struct GameReport {
-    game_pos: GamePos,
-    empty_spaces: Vec<u8>,
-    winner: Player,
-    x_winning_moves: Vec<u8>,
-    o_winning_moves: Vec<u8>,
-}
-
-#[allow(dead_code)]
-fn create_id(pos: &GamePos) -> u32 {
-    let mut id: u32 = 0;
-
-    for i in 0..pos.len() {
-        id += (pos[i] as u32) * 10_u32.pow(i as u32);
-    }
-
-    return id;
-}
-
-#[allow(dead_code)]
 fn find_winner(pos: &GamePos) -> Player {
     let mut player_count: u8 = 0;
 
@@ -44,10 +26,12 @@ fn find_winner(pos: &GamePos) -> Player {
             player_count += pos[WINNING_PATTERNS[i][j] as usize];
         }
 
-        match player_count {
-            6 => return X,
-            9 => return O,
-            _ => player_count = 0,
+        if player_count == X * 3 {
+            return X;
+        } else if player_count == O * 3 {
+            return O;
+        } else {
+            player_count = 0;
         }
     }
 
@@ -77,7 +61,7 @@ fn find_player_winning_moves(pos: &GamePos, player: Player) -> Vec<u8> {
             player_count += pos[WINNING_PATTERNS[i][j] as usize];
         }
 
-        if player * 2 == player_count - 1 {
+        if player * 2 == player_count {
             for j in 0..3 {
                 if pos[WINNING_PATTERNS[i][j] as usize] == NONE {
                     winning_moves.push(WINNING_PATTERNS[i][j] as u8);
@@ -91,13 +75,15 @@ fn find_player_winning_moves(pos: &GamePos, player: Player) -> Vec<u8> {
     return winning_moves;
 }
 
-#[allow(dead_code)]
-fn build_game_report(pos: &GamePos) -> GameReport {
-    return GameReport {
-        game_pos: pos.clone(),
-        empty_spaces: find_empty_spaces(&pos),
-        winner: find_winner(&pos),
-        x_winning_moves: find_player_winning_moves(&pos, X),
-        o_winning_moves: find_player_winning_moves(&pos, O),
-    };
-}
+// #[allow(dead_code)]
+// fn rating_all_moves(pos: &GamePos, player: Player) -> HashMap<GamePos, u32> {
+//     let mut rated_moves: HashMap<GamePos, u32> = HashMap::new();
+//     let mut waiting_stack: Vec<GamePos> = vec![pos.clone()];
+//     let in_prosess_pos: &GamePos = &waiting_stack[waiting_stack.len() - 1];
+
+//     while !waiting_stack.is_empty() {
+
+//     }
+
+//     return rated_moves;
+// }
